5a247591905a21f9bbe0cc4281fe21722e84a3aa
Yu Qiao <qiaoyu.yu@gmail.com>
2013-04-27 13:57:05 -0400

    bug fix

--- utilities/twitter-preprocess/src/edu/columbia/watson/twitter/Preprocessor.java
+++ utilities/twitter-preprocess/src/edu/columbia/watson/twitter/Preprocessor.java
@@ -21,25 +21,25 @@ import com.cybozu.labs.langdetect.LangDetectException;
 //tweet id, userid, time, hashtag(null), location(null), url(null), tweet
 public class Preprocessor {
 	private static String urlRegex = "\\(?\\b(http://|www[.])[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|]";
-	
+
 	public static String parseHTML(String rawText)
 	{
-		 return Jsoup.parse(rawText).text();
+		return Jsoup.parse(rawText).text();
 	}
-	
+
 	public static void initLanguageDetector() throws LangDetectException
 	{
 		DetectorFactory.loadProfile("./profiles");
 	}
-	
+
 	public static void main(String args[])
 	{
 		if (args.length < 1){
 			System.err.println("Usage: run.sh edu.columbia.watson.twitter.Preprocessor filename");
 			return;
 		}
-		
-		
+
+
 		try {
 			initLanguageDetector();
 		} catch (LangDetectException e1) {
@@ -47,82 +47,82 @@ public class Preprocessor {
 			e1.printStackTrace();
 			return;
 		}
-		
-		
+
+
 		BufferedReader in;
 		try {
 			in = new BufferedReader(new FileReader(args[0]));
 			while (in.ready()) {	
-			  String line = in.readLine();
-			  String splitted[] = line.split("\t");
-			  String id = splitted[0];
-			  String user = splitted[1];
-			  String rawTweet = splitted[2];
-			  String time = splitted.length >= 4 ? splitted[3] : "";
-			  
-			  //detect language
-			  Detector detector;
-			try {
-				detector = DetectorFactory.create();
-				detector.append(rawTweet);
-				if (!detector.detect().equals("en"))
-					continue;
-				//System.out.println(detector.detect());
-			} catch (LangDetectException e) {
-				//swallow the exception
-			}
-			 		  
-			  //extract geo location
-			  String location = "";
-			  if (splitted.length >= 5){
-				  int index = splitted[4].lastIndexOf("from");
-				  location = index != -1 ? splitted[4].substring(index + 5) : "";
-			  }
-			  
-			  String tweet = parseHTML(rawTweet);	//remove HTML tags
-			  
-			  //eliminate retweets
-			  if (tweet.length() >= 2 && tweet.charAt(0) == 'R' && tweet.charAt(1) == 'T')
-				  continue;
-			  
-			  String hashTag = "";
-			  String[] tweetSplitted = tweet.split(" ");
-			  tweet = "";	//ready to reuse
-			  for (String tweetPart : tweetSplitted){		//remove @ tags and extract # tags
-				  if (tweetPart.length() <= 0)
-					  break;
-				  boolean all_number = true;
-				  //remove numbers
-				  for (int i = 0 ; i < tweetPart.length() ; ++i)
-					  if (!('0' <= tweetPart.charAt(i) && tweetPart.charAt(i) <= '9')){
-						  all_number = false;
-						  break;
-					  }
-				  if (all_number)
-					  continue;
-				  char firstChar = tweetPart.charAt(0);
-				  if (firstChar == '#'){
-					  //tweet += tweetPart.substring(1);
-					  hashTag = tweetPart;
-				  }
-				  else if (firstChar != '@')
-					  tweet += tweetPart + " ";
-			  }			  
+				String line = in.readLine();
+				String splitted[] = line.split("\t");
+				String id = splitted[0];
+				String user = splitted[1];
+				String rawTweet = splitted[2];
+				String time = splitted.length >= 4 ? splitted[3] : "";
 
-			  //extract URLs
-			  //http://blog.houen.net/java-get-url-from-string/
-			  Pattern p = Pattern.compile(urlRegex);
-			  Matcher m = p.matcher(tweet);
-			  String urlStr = "";
-			  if (m.find()) {
-				  urlStr = m.group();
-				  if (urlStr.startsWith("(") && urlStr.endsWith(")")){
-					  urlStr = urlStr.substring(1, urlStr.length() - 1);
-				  }
-			  }
-			  if (id == "")
-				  continue;
-			  System.out.println(id + "\t" + user + "\t" +  time + "\t" + hashTag + "\t" + location + "\t" + urlStr + "\t" + tweet + "\t");
+				//detect language
+				Detector detector;
+				try {
+					detector = DetectorFactory.create();
+					detector.append(rawTweet);
+					if (!detector.detect().equals("en"))
+						continue;
+					//System.out.println(detector.detect());
+				} catch (LangDetectException e) {
+					//swallow the exception
+				}
+
+				//extract geo location
+				String location = "";
+				if (splitted.length >= 5){
+					int index = splitted[4].lastIndexOf("from");
+					location = index != -1 ? splitted[4].substring(index + 5) : "";
+				}
+
+				String tweet = parseHTML(rawTweet);	//remove HTML tags
+
+				//eliminate retweets
+				if (tweet.length() >= 2 && tweet.charAt(0) == 'R' && tweet.charAt(1) == 'T')
+					continue;
+
+				String hashTag = "";
+				String[] tweetSplitted = tweet.split(" ");
+				tweet = "";	//ready to reuse
+				for (String tweetPart : tweetSplitted){		//remove @ tags and extract # tags
+					if (tweetPart.length() <= 0)
+						break;
+					boolean all_number = true;
+					//remove numbers
+					for (int i = 0 ; i < tweetPart.length() ; ++i)
+						if (!('0' <= tweetPart.charAt(i) && tweetPart.charAt(i) <= '9')){
+							all_number = false;
+							break;
+						}
+					if (all_number)
+						continue;
+					char firstChar = tweetPart.charAt(0);
+					if (firstChar == '#'){
+						//tweet += tweetPart.substring(1);
+						hashTag = tweetPart;
+					}
+					else if (firstChar != '@')
+						tweet += tweetPart + " ";
+				}			  
+
+				//extract URLs
+				//http://blog.houen.net/java-get-url-from-string/
+				Pattern p = Pattern.compile(urlRegex);
+				Matcher m = p.matcher(tweet);
+				String urlStr = "";
+				if (m.find()) {
+					urlStr = m.group();
+					if (urlStr.startsWith("(") && urlStr.endsWith(")")){
+						urlStr = urlStr.substring(1, urlStr.length() - 1);
+					}
+				}
+				if (id == "")
+					continue;
+				System.out.println(id + "\t" + user + "\t" +  time + "\t" + hashTag + "\t" + location + "\t" + urlStr + "\t" + tweet + "\t");
 			}
 			in.close();
 		} catch (IOException e) {
