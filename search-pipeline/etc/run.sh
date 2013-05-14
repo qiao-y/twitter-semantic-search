6ab6fadf45e83821f22d67fdd765dbed37d2f3c6
Yu Qiao <qiaoyu@ip-10-212-71-144.ec2.internal>
2013-05-14 00:06:32 -0400

    fix build.xml

--- search-pipeline/etc/run.sh
+++ search-pipeline/etc/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xmx8g -classpath "/home/qiaoyu/latest/twitter-semantic-search/search-pipeline/build/twitter-semantic-search.jar:/home/qiaoyu/latest/twitter-semantic-search/search-pipeline/lib/*" $1 $2 $3 $4 $5 $6 $7 $8 $9 
+java -Xmx7g -classpath "/home/qiaoyu/latest/twitter-semantic-search/search-pipeline/lib/*:/home/qiaoyu/latest/twitter-semantic-search/search-pipeline/build/twitter-semantic-search.jar" $1 $2 $3 $4 $5 $6 $7 $8 $9
