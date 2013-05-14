e6229141028336abe4ed52f77dabb4f1eb8fd251
Yu Qiao <qiaoyu.yu@gmail.com>
2013-05-14 14:46:36 -0400

    refinement

--- search-pipeline/src/edu/columbia/watson/twitter/util/TempJoiner.java
+++ search-pipeline/src/edu/columbia/watson/twitter/util/TempJoiner.java
@@ -1,5 +1,10 @@
 package edu.columbia.watson.twitter.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -10,10 +15,18 @@ import org.apache.mahout.common.iterator.sequencefile.PathType;
 import org.apache.mahout.common.iterator.sequencefile.SequenceFileDirIterable;
 
 public class TempJoiner {
-	static void main(String[] args){
+	
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException{
+		
 		String fileName = "/mnt/corpus/rowid_tweetid/docIndex";
 		Configuration conf = new Configuration();
-
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		Connection conn = DriverManager.getConnection(GlobalProperty.getInstance().getMySqlConnectionString(),
+				GlobalProperty.getInstance().getMySqlUserName(),GlobalProperty.getInstance().getMySqlPassword());
+		int count = 0;
+		String query = "INSERT INTO twitterid_rowid_mapping (twitter_id, row_id) VALUES(?,?)";
+		PreparedStatement pstmt = conn.prepareStatement(query);
+			
 		for (Pair<IntWritable,Text> record :
 			new SequenceFileDirIterable<IntWritable,Text>(new Path(fileName),
 					PathType.LIST,
@@ -24,7 +37,16 @@ public class TempJoiner {
 
 			Integer rowID = record.getFirst().get();
 			Long tweetID = Long.parseLong(record.getSecond().toString());
-			System.out.println(rowID + "->" + tweetID);
+			pstmt.setLong(1, tweetID);
+			pstmt.setInt(2, rowID);
+			pstmt.execute();
+			
+			if (count++ % 10000 == 0){
+				System.out.println(count + " done");
+			}
+			
+			
+			//System.out.println(rowID + "->" + tweetID);
 		}
 	}
 }
